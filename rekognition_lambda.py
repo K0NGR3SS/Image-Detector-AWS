b09b951cb8c4c279a917f29dac8f8c8ac140d8c9
K0NGR3SS <157826941+K0NGR3SS@users.noreply.github.com>
2025-08-10 16:27:22 +0100

    Uploaded rekognition Lambda function

---
+++ rekognition_lambda.py
@@ -0,0 +1,34 @@
+import boto3
+import json
+
+def lambda_handler(event, context):
+
+    #Initializing all the clients
+    s3_client = boto3.client('s3')
+    rekognition_client = boto3.client('rekognition')
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table('ImageAnalysisResult')
+
+    #Get the S3 bucket name and object key from the event
+    bucket_name = event['Records'][0]['s3']['bucket']['name']
+    object_key = event['Records'][0]['s3']['object']['key']
+
+    #Call Amazon Rekognition to detect labels in the image
+    response = rekognition_client.detect_labels(
+        Image={'S3Object': {'Bucket': bucket_name, 'Name': object_key}},
+        MaxLabels=10
+    )
+
+    #Store the labels detected in DynamoDB
+    labels = [{'Confidence': label['Confidence'], 'Name': label['Name']} for label in response['Labels']]
+    table.put_item(
+        Item={
+            'ImageName': object_key,
+            'Labels': json.dumps(labels)
+        }
+    )
+
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Image processed successfully!')
+    }
